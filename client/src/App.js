12e2ed703553e76fb3d9b8274a116a02e99205f3
steffilamarca <174576295+steffilamarca@users.noreply.github.com>
2024-07-19 18:36:09 +0800

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import React from 'react';
-import VaccineList from './components/VaccineList';
+import React, { useState } from 'react';
+import View from './components/View';
 
 const App = () => (
-    <div>
-        <VaccineList />
+    <div className='container'>
+        <div className='row'>
+            <div className='col-12'>
+                <h3>Welcome to</h3>
+                <h1>VacciPROFILE</h1>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-3'></div>
+            <div className='col-9'></div>
+        </div>
     </div>
 );
 
