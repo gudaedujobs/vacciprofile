d12eaa3dd10f7bd2279910d255bb17baaec3932d
steffilamarca <174576295+steffilamarca@users.noreply.github.com>
2024-07-22 16:17:25 +0800

    Latest working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.scss';
 // import manufacturers from './assets/data/manufacturers.json';
 import viruses from './assets/data/viruses.json';
 import vaccines from './assets/data/vaccines.json';
@@ -80,7 +80,7 @@ const App = () => {
                 </div>
             </div>
             <div className='row mt-4'>
-                <div className='sidebar col-4 col-lg-3'>
+                <div className='sidebar col-6 col-sm-4 col-lg-3'>
                     <div className='search-container'>
                         <span className="position-relative">
                             <input 
@@ -96,11 +96,11 @@ const App = () => {
                     </div>
                     <div className='virus-list mt-3'>
                         {viruses.map((v, i) => (
-                            <div key={i} className='virus-list-item bg-light' onClick={() => handleSelectVirus(v)}>{v.name}</div>
+                            <div key={i} className='virus-list-item bg-light text-dark rounded-3 py-1 mt-2' onClick={() => handleSelectVirus(v)}>{v.name}</div>
                         ))}
                     </div>
                 </div>
-                <div className='view-container col-8 col-lg-9 border border-primary border-1 rounded-4 p-0'>
+                <div className='view-container col-6 col-sm-8 col-lg-9 border border-primary border-1 rounded-4 p-0'>
                     <h1 className='heading text-primary px-3 pt-2'>Updated Virus-Reported Data</h1>
                     <div className='view-header table-responsive m-0'>
                         <table className='table table-success w-100 m-0'>
@@ -116,12 +116,12 @@ const App = () => {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td className='hover-underline'>{selectedVirus.name}</td>
-                                    <td className='hover-underline'>{getVaccineNames(selectedVirus.vaccines)}</td>
-                                    <td className='hover-underline'>{selectedVirus.vaccines?.[0]?.vaccineId ? getCountriesForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
-                                    <td className='hover-underline'>{selectedVirus.vaccines?.[0]?.vaccineId ? getManufacturerForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
-                                    <td className='hover-underline'>{selectedVirus.vaccines?.[0]?.vaccineId ? getAccreditationsForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
-                                    <td className='hover-underline'>{selectedVirus.vaccines?.[0]?.vaccineId ? getRecommendationForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
+                                    <td className='hover-highlight'>{selectedVirus.name}</td>
+                                    <td className=''>{getVaccineNames(selectedVirus.vaccines)}</td>
+                                    <td className='hover-highlight'>{selectedVirus.vaccines?.[0]?.vaccineId ? getCountriesForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
+                                    <td className='hover-highlight'>{selectedVirus.vaccines?.[0]?.vaccineId ? getManufacturerForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
+                                    <td className='hover-highlight'>{selectedVirus.vaccines?.[0]?.vaccineId ? getAccreditationsForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
+                                    <td className=''>{selectedVirus.vaccines?.[0]?.vaccineId ? getRecommendationForVaccine(selectedVirus.vaccines[0].vaccineId) : '-'}</td>
                                 </tr> 
                             </tbody>
                         </table>
