48f47599a176a133dc3d5fc6cfdab91fabbde2e5
steffilamarca <174576295+steffilamarca@users.noreply.github.com>
2024-07-22 11:26:26 +0800

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
-import View from './components/View';
+import './App.css';
+import manufactuers from './assets/data/manufacturers.json';
 
-const App = () => (
-    <div className='container'>
-        <div className='row'>
+const App = () => {
+
+    const [ activeManufacturer , setActiveManufacturer ] = useState({});
+
+    const handleSearch = keyword => {}
+
+    return <div className='container'>
+        <div className='row bg-primary text-white py-4'>
             <div className='col-12'>
-                <h3>Welcome to</h3>
-                <h1>VacciPROFILE</h1>
+                <h3 className='subheading mb-0'>Welcome to</h3>
+                <h1 className='heading'>VacciPROFILE</h1>
             </div>
         </div>
-        <div className='row'>
-            <div className='col-3'></div>
-            <div className='col-9'></div>
+        <div className='row mt-4'>
+            <div className='sidebar col-3'>
+                <div className='search-container'>
+                    <span className="position-relative">
+                        <input type="text" className="text-center bg-info rounded-2 border-dark border-0" id="search" name="search" placeholder="Search" onChange={e=>handleSearch(e.target.value)}/>
+                        <i className="fa fa-search position-absolute top-50 translate-middle-y end-0 me-2 text-muted" aria-hidden="true"></i>
+                    </span>
+                </div>
+                <div className='manufacturer-list'>
+                    {manufactuers.map((m,i)=><div key={i}>{m.name}</div>)}
+                </div>
+            </div>
+            <div className='view-container col-9 border border-primary border-1 rounded-0 p-0'>
+                <h1 className='heading text-primary px-3 pt-2'>Updated Virus-Reported Data</h1>
+                <div className='view-header row bg-primary m-0'>
+                    <div className='col-2'></div>
+                    <div className='col-2'></div>
+                    <div className='col-2'></div>
+                    <div className='col-2'></div>
+                    <div className='col-2'></div>
+                    <div className='col-2'></div>
+                </div>
+            </div>
         </div>
     </div>
-);
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/custom.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
